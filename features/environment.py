8e8158384153e3280930db42143bd19f93931a00
Nigel Charman <nigel.charman.nz@gmail.com>
2023-01-27 17:46:48 +1300

    Change timezone rather than locale

--- features/environment.py
+++ features/environment.py
@@ -13,7 +13,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #  ====================================================================
-import locale
+import os
 
 from behave import fixture
 from inat_to_cams import cams_interface, cams_reader, cams_writer, synchronise_inat_to_cams
@@ -26,5 +26,5 @@ def before_all(context):
     context.synchroniser = synchronise_inat_to_cams.synchroniser
     context.connection = cams_interface.connection
     context.connection.delete_rows_with_inat_ref_of_length(4)
-    locale.setlocale('en-NZ')
+    os.environ['TZ'] = 'Pacific/Auckland'
     # logging.basicConfig(filename="error.log")
